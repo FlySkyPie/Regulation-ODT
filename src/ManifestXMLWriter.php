f213210bcf9ec3a216074b1a52e3710474d3d950
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-02-17 00:06:18 +0800

    add ManifestXMLWriter.

---
+++ src/ManifestXMLWriter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace FlySkyPie\RegulationODText;
+
+use FlySkyPie\RegulationODText\XMLWriter;
+
+/**
+ * Description of ManifestXMLWriter
+ *
+ * @author flyskypie
+ */
+class ManifestXMLWriter extends XMLWriter {
+
+  public function __construct() {
+    parent::__construct();
+    $this->createRootElement();
+  }
+
+  private function createRootElement() {
+    $attribute = [
+        'manifest:version' => '1.2',
+        'xmlns:manifest' => 'urn:oasis:names:tc:opendocument:xmlns:manifest:1.0'
+    ];
+
+    $rootElement = $this->createNewElement('manifest:manifest', $attribute);
+    $this->addEntries($rootElement);
+    $this->getDocument()->appendChild($rootElement);
+  }
+
+  private function addEntries(\DOMElement $parent) {
+    $attributes = [
+        ['manifest:media-type' => 'application/vnd.oasis.opendocument.text',
+            'manifest:full-path' => '/',
+            'manifest:version' => '1.2'],
+        ['manifest:media-type' => 'text/xml', 'manifest:full-path' => 'content.xml'],
+        ['manifest:media-type' => 'text/xml', 'manifest:full-path' => 'meta.xml'],
+        ['manifest:media-type' => 'text/xml', 'manifest:full-path' => 'styles.xml']
+    ];
+    foreach ($attributes as $attribute) {
+      $this->addEntry($parent, $attribute);
+    }
+  }
+
+  private function addEntry(\DOMElement $parent, array $attribute) {
+    $element = $this->createNewElement('manifest:file-entry', $attribute);
+    $parent->appendChild($element);
+  }
+
+}
---
+++ src/XMLWriter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FlySkyPie\RegulationODText;
+
+/**
+ * Description of XMLWriter
+ *
+ * @author flyskypie
+ */
+class XMLWriter {
+
+  private $domDocument;
+
+  public function __construct() {
+    $this->domDocument = new \DomDocument('1.0', 'UTF-8');
+  }
+  
+  public function getString():string{
+    return $this->domDocument->saveXML();
+  }
+
+  protected function getDocument(): \DomDocument {
+    return $this->domDocument;
+  }
+
+  /**
+   * 
+   * @param string $tagName
+   * @param array $attributes
+   * @return \DOMElement
+   */
+  protected function createNewElement(string $tagName, array $attributes): \DOMElement {
+    $newNode = $this->domDocument->createElement($tagName);
+    foreach ($attributes as $key => $value) {
+      $newNode->setAttribute($key, $value);
+    }
+    return $newNode;
+  }
+
+}
